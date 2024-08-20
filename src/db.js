f260688e67689d977c24b3b3f980069e3d5eb373
DarkSevenX <nargumedor@unicartagena.edu.co>
2024-08-19 23:11:04 -0500

    añadiendo libsql para conexion con turso

--- src/db.js
+++ src/db.js
@@ -1,6 +1,14 @@
 
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
-const prisma = new PrismaClient()
+const libsql = createClient({
+    url: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  });
+
+const adapter = new PrismaLibSQL(libsql);
+const prisma = new PrismaClient({ adapter })
 
 export { prisma }
